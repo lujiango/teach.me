eeed536c9f97b3ddf9d1713a619176fc45299c03
lujiango <lujiang200829@126.com>
2016-04-16 11:08:07 +0800

    add tlog server

---
+++ teaching/tlog/tlog.go
@@ -0,0 +1,39 @@
+package tlog
+
+import (
+	"log"
+	"os"
+)
+
+var logName string = "teaching.log"
+var isInit bool = false
+var logger *log.Logger
+
+func initLog(prefix string) {
+	if !isInit {
+		logFile, err := os.OpenFile(logName, os.O_RDWR|os.O_APPEND, os.ModeAppend)
+		if err != nil {
+			log.Fatalln("open log file error !")
+		}
+		logger = log.New(logFile, prefix, log.LstdFlags|log.Lshortfile)
+		if logger != nil {
+			isInit = true
+		}
+	} else {
+		logger.SetPrefix(prefix)
+	}
+}
+func Info(v ...interface{}) {
+	initLog("[INFO]")
+	logger.Println(v)
+}
+
+func Debug(v ...interface{}) {
+	initLog("[DEBUG]")
+	logger.Println(v)
+
+}
+func Error(v ...interface{}) {
+	initLog("[ERROR]")
+	logger.Println(v)
+}
