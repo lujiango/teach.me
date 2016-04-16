c290077fbee2d97b5182eba083c65b39589ff140
lujiango <lujiang200829@126.com>
2016-04-16 15:49:09 +0800

    fix tlog bugs

--- teaching/tlog/tlog.go
+++ teaching/tlog/tlog.go
@@ -1,19 +1,20 @@
 package tlog
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
 
-var logName string = "teaching.log"
+var logName string = "tlog/teaching.log"
 var isInit bool = false
 var logger *log.Logger
 
 func initLog(prefix string) {
 	if !isInit {
-		logFile, err := os.OpenFile(logName, os.O_RDWR|os.O_APPEND, os.ModeAppend)
+		logFile, err := os.OpenFile(logName, os.O_RDWR|os.O_APPEND|os.O_CREATE, os.ModeAppend)
 		if err != nil {
-			log.Fatalln("open log file error !")
+			log.Fatalln("open log file error !", err)
 		}
 		logger = log.New(logFile, prefix, log.LstdFlags|log.Lshortfile)
 		if logger != nil {
@@ -25,19 +26,18 @@ func initLog(prefix string) {
 }
 func Info(v ...interface{}) {
 	initLog("[INFO]")
-	logger.Println(v)
+	logger.Output(2, fmt.Sprintln(v...))
 }
 
 func Debug(v ...interface{}) {
 	initLog("[DEBUG]")
-	logger.Println(v)
-
+	logger.Output(2, fmt.Sprintln(v...))
 }
 func Error(v ...interface{}) {
 	initLog("[ERROR]")
-	logger.Println(v)
+	logger.Output(2, fmt.Sprintln(v...))
 }
 func Fatal(v ...interface{}) {
 	initLog("[FATAL]")
-	logger.Fatalln(v)
+	logger.Output(2, fmt.Sprintln(v...))
 }
