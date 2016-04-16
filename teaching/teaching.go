98a04ffe2c54e7f1976239c9d82674a1dbd03e7f
lujiango <lujiang200829@126.com>
2016-04-16 15:50:52 +0800

    add tlog for teaching project

--- teaching/teaching.go
+++ teaching/teaching.go
@@ -2,13 +2,15 @@
 package main
 
 import (
+	"teach.me/teaching/conf"
 	"teach.me/teaching/http"
 	"teach.me/teaching/tlog"
 )
 
 func main() {
 	tlog.Info(">>> Teaching server started...")
-
+	conf.SetConfig()
+	tlog.Info(conf.Gconfig)
 	http.Router()
 	http.Start()
 }
