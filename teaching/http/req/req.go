ebbf51db7c511925ee8110920f482643f235d60a
lujiango <lujiang200829@126.com>
2016-05-15 18:34:25 +0800

    add new file

---
+++ teaching/http/req/req.go
@@ -0,0 +1,9 @@
+package req
+
+const (
+	TEACHING_INDEX                 = "/teaching/index"
+	TEACHING_HTML_TEST             = "/teaching/html/test"
+	TEACHING_USER_BASE_REG         = "/teaching/user/base/_reg"
+	TEACHING_USER_BASE_LOGIN       = "/teaching/user/base/_login"
+	TEACHING_USER_BASE_PHONE_CHECK = "/teaching/user/base/phone/_check"
+)
